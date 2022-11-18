688611ffd9f14148f49e4bab8620f30fc2e979a5
manu4243 <tejada.enmanuel@gmail.com>
2022-11-18 14:59:56 -0400

    Adding Files

---
+++ section_3/Delimeters.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Delimeters {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter two integers on the same line: ");
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+        System.out.println("Please, write a sentence.");
+        String num = scan.nextLine();
+        System.out.println(num);
+        System.out.println("Please enter two big numbers: ");
+        long bignum1 = scan.nextLong();
+        // System.out.println(num1);
+        // System.out.println(num2);
+        System.out.println(bignum1);
+        System.out.println("Please enter a sentence: ");
+
+    }
+    
+}
