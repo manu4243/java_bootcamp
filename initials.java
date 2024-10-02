3c1bfc2e9a15b0ae25d4aa8b5ac21fb9aa7d052a
manu4243 <tejada.enmanuel@gmail.com>
2024-10-02 16:51:54 -0400

    Adding resources

---
+++ initials.java
@@ -0,0 +1,11 @@
+public class initials {
+    public static void main(String[] args) {
+        System.out.println("EEEEEE    TTTTTTTTTTTT");
+        System.out.println("E              T");
+        System.out.println("E              T");
+        System.out.println("EEEEEE         T");
+        System.out.println("E              T");
+        System.out.println("E              T");
+        System.out.println("EEEEEE         T");
+    }
+}
---
+++ section_2/variables2.java
@@ -0,0 +1,12 @@
+public class variables2 {
+    public static void main(String[] args) {
+        int passengers = 7;
+        System.out.println(passengers);
+        passengers += 13;
+        System.out.println("En el bus van " + passengers + " personas.");
+        int busTickets = passengers;
+        System.out.println(busTickets);
+        long maxValue = 2147483648L;
+        System.out.println(maxValue);
+    }
+}
